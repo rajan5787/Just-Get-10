93f459f36b5719fe970e427868c0484ea1d6abab
rajan5787 <rajandp36@gmail.com>
2018-06-21 00:56:06 +0530

    MainActivity and MainPage UI complete

--- app/src/main/java/rajan5787/tikuraja/justget10/MainActivity.java
+++ app/src/main/java/rajan5787/tikuraja/justget10/MainActivity.java
@@ -1,13 +1,28 @@
 package rajan5787.tikuraja.justget10;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStartGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStartGame = findViewById(R.id.btn_start_game);
+
+        btnStartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MainPage.class);
+                startActivity(i);
+            }
+        });
     }
 }
